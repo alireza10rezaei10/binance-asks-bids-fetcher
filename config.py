ed855e85b48b3450e709f7f99214dd65db8b8827
Alireza Rezaei <Alireza10Rezaei10@gmail.com>
2025-07-18 03:49:00 +0330

    remove unusefull variables from config

--- config.py
+++ config.py
@@ -10,7 +10,6 @@ SYMBOLS: list[str] = ["btcusdt", "ethusdt", "solusdt", "bnbusdt"]
 ORDERBOOK_SAVING_METHOD = OrderBookSavingMethods.ESSENTIAL_UPDATES
 SAVE_DIR: str = "orderbook_data"
 
-DEPTH_URL_TEMPLATE: str = "wss://fstream.binance.com/stream?streams={}@depth"
 QUEUE_MAXSIZE: int = 1000
 FLUSH_INTERVAL: int = 5  # seconds
 MAX_BATCH_SIZE: int = 500
