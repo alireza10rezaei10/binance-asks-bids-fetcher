a7ae16ee501518fdc7be1270cc3f39c2c1fa1914
Alireza Rezaei <Alireza10Rezaei10@gmail.com>
2025-07-18 21:47:06 +0330

    set UTC timing

--- src/logger_config.py
+++ src/logger_config.py
@@ -1,7 +1,10 @@
 import logging
+import time
 
 
 def setup_logger() -> logging.Logger:
+    logging.Formatter.converter = time.gmtime
+
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] [%(name)s] %(message)s",
